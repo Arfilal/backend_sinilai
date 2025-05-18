ca19f8c177f9f27fa4e4d528d448633dc521fc7e
Arfilal <arfilalfaiznadi04@gmail.com>
2025-05-18 22:04:15 +0700

    baru 36

--- app/Controllers/MahasiswaController.php
+++ app/Controllers/MahasiswaController.php
@@ -1,27 +1,9 @@
-<?php
-namespace App\Controllers;
-
-use App\Models\MahasiswaModel;
-use CodeIgniter\API\ResponseTrait;
-
-class MahasiswaController extends BaseController
 {
-    use ResponseTrait;
-
-    public function getMahasiswaByNpm($npm)
-    {
-        $mahasiswaModel = new MahasiswaModel();
-
-        $mahasiswa = $mahasiswaModel
-            ->select('mahasiswa.npm, mahasiswa.nama, prodi.nama_prodi')
-            ->join('prodi', 'prodi.id = mahasiswa.prodi_id')
-            ->where('mahasiswa.npm', $npm)
-            ->first();
-
-        if ($mahasiswa) {
-            return $this->respond($mahasiswa);
-        } else {
-            return $this->failNotFound('Data tidak ditemukan');
-        }
-    }
+	"info": {
+		"_postman_id": "d00bd89e-bbc9-4695-b220-5bf69b85e88f",
+		"name": "login.postman_collection.json",
+		"schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+		"_exporter_id": "42769259"
+	},
+	"item": []
 }
