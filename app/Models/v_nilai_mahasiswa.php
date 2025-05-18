db2a3548ab0afe4c9186cddc459bb10af22ab732
Arfilal <arfilalfaiznadi04@gmail.com>
2025-05-18 20:56:50 +0700

    baru 33

--- app/Models/v_nilai_mahasiswa.php
+++ app/Models/v_nilai_mahasiswa.php
@@ -6,7 +6,9 @@ use CodeIgniter\Model;
 
 class v_nilai_mahasiswa extends Model
 {
-    protected $table = 'v_nilai_mahasiswa';  // sesuaikan dengan nama view yang ada di DB
-    protected $primaryKey = '';  // sesuaikan primary key jika ada
-    protected $allowedFields = ['Kode Mata Kuliah', 'Nama Mata Kuliah', 'Jumlah SKS', 'Nilai', 'Status Kelulusan']; // sesuaikan kolom yang boleh diisi
+    protected $table = 'v_nilai_mahasiswa';
+    protected $primaryKey = 'id_dummy'; // bisa isi kolom apa saja yg unik (atau bahkan fiktif jika hanya untuk bypass error)
+    protected $useAutoIncrement = false;
+    protected $returnType = 'array';
 }
+
